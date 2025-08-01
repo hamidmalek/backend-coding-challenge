46f67649d353fe1646808f7275ec044bd69186ed
Hamid Asus <hmdmlkv@gmail.com>
2025-08-01 18:04:27 +0200

    init Spring Boot.

---
+++ src/test/kotlin/com/example/backendcodingchallenge/BackendCodingChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backendcodingchallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendCodingChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
